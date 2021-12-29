df86e2395a76982cccec507e7b0f4cea5719cb5d
jonny.alves <jonny.alves@zup.com.br>
2021-12-29 20:40:18 -0300

    passando informacoes do controller para a view

--- src/main/java/br/com/ediarista/controller/HomeController.java
+++ src/main/java/br/com/ediarista/controller/HomeController.java
@@ -1,8 +1,11 @@
 package br.com.ediarista.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
@@ -11,5 +14,20 @@ public class HomeController {
     @RequestMapping(name = "/", method = RequestMethod.GET)
     public String home(){
         return "home";
+    }
+
+    @GetMapping("/modelMap")
+    public String home2(ModelMap modelMap){
+        modelMap.addAttribute("nome", "José");
+        return "home";
+    }
+
+    @GetMapping("/modelAndView")
+    public ModelAndView home3(){
+        var modelAndView = new ModelAndView("home");
+
+        modelAndView.addObject("nome", "José");
+
+        return modelAndView;
     }
 }
