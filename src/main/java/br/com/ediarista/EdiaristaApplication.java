b551ef182ed099c34fa48047dc7f6a1100203aa9
jonny.alves <jonny.alves@zup.com.br>
2021-12-29 20:14:27 -0300

    commit inicial

---
+++ src/main/java/br/com/ediarista/EdiaristaApplication.java
@@ -0,0 +1,13 @@
+package br.com.ediarista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdiaristaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdiaristaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ediarista/EdiaristaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ediarista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdiaristaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
